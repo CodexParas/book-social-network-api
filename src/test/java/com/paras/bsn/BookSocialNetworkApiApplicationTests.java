a905aed4935eafaf935d8485be6a6acd21022b90
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-08-16 09:47:35 +0530

    Initial commit

---
+++ src/test/java/com/paras/bsn/BookSocialNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paras.bsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
