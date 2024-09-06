a3abf204160b288031fecdf16fdd8804c770da98
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-09-07 00:55:41 +0530

    Adds register flow

---
+++ src/main/java/com/paras/bsn/auth/AuthenticationService.java
@@ -0,0 +1,86 @@
+package com.paras.bsn.auth;
+
+import com.paras.bsn.email.EmailService;
+import com.paras.bsn.email.EmailTemplateName;
+import com.paras.bsn.role.RoleRepository;
+import com.paras.bsn.user.Token;
+import com.paras.bsn.user.TokenRepository;
+import com.paras.bsn.user.User;
+import com.paras.bsn.user.UserRepository;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+
+    @Value("${activation-url}")
+    private String activationUrl;
+
+    public void register(@Valid RegisterRequest registerRequest) throws MessagingException {
+        var userRole = roleRepository.findByName("USER").orElseThrow(
+                () -> new IllegalStateException("Role USER was not initiated")
+        );
+        var user = User.builder()
+                .firstName(registerRequest.getFirstName())
+                .lastName(registerRequest.getLastName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+
+    }
+
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+        emailService.sendEmail(
+                user.getEmail(),
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                "Activate your account",
+                newToken
+        );
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+}
