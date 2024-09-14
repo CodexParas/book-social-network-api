4c0f123bd786bc0bd26dd87509de6fde37c5fbb5
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-09-14 10:55:44 +0530

    Adds whole authentication, account verification and registration module

--- src/main/java/com/paras/bsn/auth/AuthenticationService.java
+++ src/main/java/com/paras/bsn/auth/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.paras.bsn.auth;
 import com.paras.bsn.email.EmailService;
 import com.paras.bsn.email.EmailTemplateName;
 import com.paras.bsn.role.RoleRepository;
+import com.paras.bsn.security.JwtService;
 import com.paras.bsn.user.Token;
 import com.paras.bsn.user.TokenRepository;
 import com.paras.bsn.user.User;
@@ -11,11 +12,15 @@ import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -27,6 +32,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
     private final EmailService emailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Value("${activation-url}")
     private String activationUrl;
@@ -82,5 +89,37 @@ public class AuthenticationService {
             codeBuilder.append(characters.charAt(randomIndex));
         }
         return codeBuilder.toString();
+    }
+
+    public AuthenticationResponse authenticate(@Valid AuthenticationRequest authenticationRequest) {
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        var claims = new HashMap<String, Object>();
+        var user = ((User) auth.getPrincipal());
+        claims.put("fullName", user.fullName());
+        var jwtToken = jwtService.generateToken(claims, user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken).build();
+    }
+
+    @Transactional
+    public void activateAccount(String token) throws MessagingException {
+        Token savedToken = tokenRepository.findByToken(token)
+                // todo check if token is expired
+                .orElseThrow(() -> new RuntimeException("Token not found"));
+        if(LocalDateTime.now().isAfter(savedToken.getExpiresAt())) {
+            sendValidationEmail(savedToken.getUser());
+            throw new RuntimeException("Token expired. A new token has been sent to your email");
+        }
+        var user = userRepository.findById(savedToken.getUser().getId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        user.setEnabled(true);
+        userRepository.save(user);
+        savedToken.setValidatedAt(LocalDateTime.now());
+        tokenRepository.save(savedToken);
     }
 }
