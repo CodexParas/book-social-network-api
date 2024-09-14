e1f1ca986d12c6f9232dcfc882350eeb808f1098
Paras Gupta <37538941+CodexParas@users.noreply.github.com>
2024-09-14 13:53:46 +0530

    Adds auditing for created by and last modified by

--- src/main/java/com/paras/bsn/BookSocialNetworkApiApplication.java
+++ src/main/java/com/paras/bsn/BookSocialNetworkApiApplication.java
@@ -10,21 +10,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class BookSocialNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookSocialNetworkApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookSocialNetworkApiApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner runner(RoleRepository roleRepository) {
-		return args -> {
-			if(roleRepository.findByName("USER").isEmpty()) {
-				roleRepository.save(Role.builder().name("USER").build());
-			}
-		};
-	}
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(Role.builder().name("USER").build());
+            }
+        };
+    }
 
 }
